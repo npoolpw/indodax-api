ae67c4f458b7762625b24075d8320f6c28b1a606
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-07-11 11:33:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cloudscraper = require('cloudscraper');
+var hmac_sha512 = require('./hmac-sha512.js');
 var Indodax = function (key, secret, family){
-  family = family || 4;
+	family = family || 4;
 	var self = this;
 
 	self.VERSION = '1.0.0';
@@ -8,10 +9,22 @@ var Indodax = function (key, secret, family){
 	self._key = key;
 	self._secret = secret;
 
-	self._endpoint = 'tradeogre.com/api/v1';
-	self._publicUrl = 'https://' + self._endpoint;
-	self._privateUrl = 'https://' + self._key + ":" + self._secret + "@" + self._endpoint;
-  self._userAgent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0';
-  
-  return self;
-}
+	self._userAgent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0';
+
+	self._get = function (data, options, callback){
+		var qs = ''
+		options = options || {};
+		for (var d in data) {
+			qs += '/' + data[d]
+		}
+		
+		options['method'] = 'GET';
+		options['url'] = 'https://indodax.com/api' + qs;
+		options['timeout'] = 2000;
+		options['headers'] = {'User-Agent': self._userAgent};
+		options['family'] = family;
+		
+		return cloudscraper.request(options,callback);
+	};
+	return self;
+};
